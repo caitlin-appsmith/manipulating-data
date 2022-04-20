775bc804fc2ca2ba6f3ecfdc9f47a570373582e5
caitlin-appsmith <caitlin@tv83.net>
2022-04-20 15:34:29 +1000

    end of session 3

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { companiesData } from "./companies-data";
+import { salesData } from "./sales-data";
 
 const extraRecord = {
   title: "JS in FM Training",
@@ -8,17 +9,59 @@ const extraRecord = {
 };
 
 const before = document.getElementById("before");
+// before.innerHTML = "Hello";
 before.innerHTML = JSON.stringify(companiesData, null, 2);
-
 //TODO: Extract an element from the array.
+// const manipulatedData = companiesData[12];
 //TODO: Add elements to beginning and end of array.
+// const manipulatedData = companiesData.pop(); //removes the last element
+//const manipulatedData = companiesData.shift(); // removes first element
+// const manipulatedData = companiesData.unshift(extraRecord); //adds extra record to start of array and returns length
+// const manipulatedData = companiesData.push(extraRecord); //adds extra record to end of array and returns length
 //TODO: Remove Elements from end of array.
 //TODO: Create a new array from old data.
+// const manipulatedData = companiesData.map(function(e){ 
+//   console.log(e)
+//   const obj = {name: e.fieldData.CompanyName, state: e.fieldData.State, suburb: e.fieldData.City, postcode: e.fieldData.Zip};
+//   return obj;
 //TODO: Filter for a certain state.
+// const manipulatedData = companiesData.filter(function(e) {
+//   return e.fieldData.State === "TX"; /// three equals is exact match
+// });
 //TODO: Find the first element with a certain name.
+// const manipulatedData = companiesData.find(function(e) {
+//   return e.fieldData.State === "TX";
+// });
 //TODO: Create a new array with just elements of a certain state.
 //TODO: Create buttons for each record.
 //functions are here
-const manipulatedData = [];
+// const manipulatedData = companiesData.find(function(e) {
+//   return e.fieldData.State === "TX";
+// });
 
+// const filterForState = function (record) {
+//   return record.fieldData.State === "TX";
+// };
+
+// const ConstructNewArray = function (e) {
+//   const obj = {
+//     company: e.fieldData.CompanyName,
+//     state: e.fieldData.State,
+//     city: e.fieldData.City,
+//   };
+//   return obj;
+// };
+//   const manipulatedData = companiesData
+//   .filter(filterForState)
+//   .map(ConstructNewArray);
+
+
+// { 
+
+//   // return e.fieldData.CompanyName
+// };
+const manipulatedData = salesData.filter(function(sale){
+  return sale.fieldData.UnitsSold >= 300})
+const after = document.getElementById("after");
 after.innerHTML = JSON.stringify(manipulatedData, null, 2);
+before.innerHTML = JSON.stringify(salesData, null, 2); //reload before
